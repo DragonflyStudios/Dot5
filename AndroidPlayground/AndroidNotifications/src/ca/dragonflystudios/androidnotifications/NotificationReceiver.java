0e559b4a01b1040dd22f61f0bdeef059e8702691
Jun Luo <jun@turix.ca>
2012-08-10 22:21:27 -0400

    adding AndroidPlayground -- an area for trying Android APIs out plus various updates of design notes

---
+++ AndroidPlayground/AndroidNotifications/src/ca/dragonflystudios/androidnotifications/NotificationReceiver.java
@@ -0,0 +1,12 @@
+package ca.dragonflystudios.androidnotifications;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationReceiver extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.result);
+  }
+} 
